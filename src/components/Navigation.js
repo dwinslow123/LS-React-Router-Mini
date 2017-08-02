bba70ea2c9baa57597676950b3859667f892a384
Dan Winslow <dwinslow123@me.com>
2017-08-02 16:45:12 -0700

    finished, with some padding

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../App.css'
 
 const Navigation = () => {
   return (
@@ -7,13 +9,13 @@ const Navigation = () => {
       <Nav className="App">
         <h1>React Router Mini</h1>
         <NavItem>
-          <NavLink href="#">Home</NavLink>
+          <Link className="link-styles" to="/">Home</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">About</NavLink>
+          <Link className="link-styles" to="/about">About</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Contact</NavLink>
+          <Link className="link-styles" to="/contact">Contact</Link>
         </NavItem>
       </Nav>
     </div>
